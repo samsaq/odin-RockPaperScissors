bb788174a8b5cfc14e3a2cad5b640e7634c75885
Sameer Reza <sameereza@gmail.com>
2023-01-11 00:26:05 -0700

    implemented buttons and UI response

--- RockPaperScissorsScripting.js
+++ RockPaperScissorsScripting.js
@@ -40,19 +40,16 @@ function whoWins(userChoice) {
     let computerChoice = getComputerChoice();
     //if the user and computer choose the same thing, it's a tie
     if (userChoice == computerChoice) {
-        console.log("It's a tie!");
         return "Tie";
     }
     //if the user chooses rock
     else if (userChoice == "Rock") {
         //if the computer chooses paper, the computer wins
         if (computerChoice == "Paper") {
-            console.log("The computer wins! Paper beats Rock!");
             return "Computer Wins";
         }
         //if the computer chooses scissors, the user wins
         else {
-            console.log("You win! Rock beats Scissors!");
             return "User Wins";
         }
     }
@@ -60,12 +57,10 @@ function whoWins(userChoice) {
     else if (userChoice == "Paper") {
         //if the computer chooses scissors, the computer wins
         if (computerChoice == "Scissors") {
-            console.log("The computer wins! Scissors beats Paper!");
             return "Computer Wins";
         }
         //if the computer chooses rock, the user wins
         else {
-            console.log("You win! Paper beats Rock!");
             return "User Wins";
         }
     }
@@ -73,21 +68,19 @@ function whoWins(userChoice) {
     else {
         //if the computer chooses rock, the computer wins
         if (computerChoice == "Rock") {
-            console.log("The computer wins! Rock beats Scissors!");
             return "Computer Wins";
         }
         //if the computer chooses paper, the user wins
         else {
-            console.log("You win! Scissors beats Paper!");
             return "User Wins";
         }
     }
 }
 
 //function to play a round
-function playRound() {
+function playRound(userChoice) {
     //use existing functions to get the user's choice and determine the winner
-    let userChoice = getUserChoice();
+    //let userChoice = getUserChoice();
     let winner = whoWins(userChoice);
 
     //update the global variables
@@ -101,32 +94,64 @@ function playRound() {
         ties++;
     }
     roundsPlayed++;
+    checkWins();
 }
 
-//function to play the game
-function playGame() {
+//function to check status of the current game & go to the win condition once 5 rounds have been played
+function checkWins() {
 
-    //explain the game and 5 round format
-    console.log("Welcome to Rock, Paper, Scissors! The game will be played in 5 rounds. The first player to win 3 rounds wins the game. Good luck!");
+    //find the score display elements
+    const playerScore = document.querySelector(".playerScore");
+    const computerScore = document.querySelector(".computerScore");
 
-    //play 5 rounds
-    for (let i = 0; i < 5; i++) {
-        playRound();
-    }
+    //update the score display elements
+    playerScore.textContent = `Player: ${userWins}`;
+    computerScore.textContent = `Computer: ${computerWins}`;
 
-    //display the results
-    console.log("You won " + userWins + " times, the computer won " + computerWins + " times, and there were " + ties + " ties.");
-    //display the winner
-    if (userWins > computerWins) {
-        console.log("You won the game!");
-    }
-    else if (userWins < computerWins) {
-        console.log("The computer won the game!");
-    }
-    else {
-        console.log("The game was a tie!");
+    if (roundsPlayed === 5) {
+        //display the winner
+        if (userWins > computerWins) {
+            displayWinner("Player");
+        }
+        else if (userWins < computerWins) {
+            displayWinner("Computer");
+        }
+        else {
+            displayWinner("Tie");
+        }
     }
 }
 
-//now play the game
-playGame();
+//function that replaces buttons and score display with winner message
+function displayWinner(winner) //winner is a string that is either "Player", "Computer", or "Tie"
+{
+    const buttonContainer = document.querySelector(".buttonContainer");
+    const scoreContainer = document.querySelector(".scoreContainer");
+
+    //remove the buttons & score display
+    buttonContainer.remove();
+    scoreContainer.remove();
+
+    //create a new div to hold the winner message
+    const winnerContainer = document.createElement("div");
+    winnerContainer.classList.add("winnerContainer");
+    //add the winner as an h2 within the div
+    const winnerMessage = document.createElement("h2");
+    winnerContainer.appendChild(winnerMessage);
+    //add the winner message
+    if(winner === "Player")
+    {
+        winner = "You";
+    }
+    else if(winner === "Computer")
+    {
+        winner = "The computer";
+    }
+    else if (winner === "Tie")
+    {
+        winner = "Nobody (It's a tie!)";
+    }
+    winnerMessage.textContent = `The winner is ${winner}!`;
+    //add the div to the body
+    document.body.appendChild(winnerContainer);
+}
